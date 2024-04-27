c2249aea6c5a5a7f21e610a008765a6d455d2761
yesdopepe <laiskashkash33@gmail.com>
2024-04-27 23:26:34 +0300

    did nothing just commiting

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,11 +4,11 @@ const Navbar = () => {
   return (
     <nav className='absolute top-4 max-w-fit px-8 h-10 backdrop-blur flex justify-around items-center rounded-3xl transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white supports-backdrop-blur:bg-white/95 dark:bg-slate-900/75'>
         <ul className='flex flex-row  gap-10'>
-            <li>hello</li>
-            <li>skills</li>
-            <li>projects</li>
-            <li>contact</li>
-            <li>blog</li>
+            <li><a href="/">hello</a></li>
+            <li><a href="/skills">skills</a></li>
+            <li><a href="/projects">projects</a></li>
+            <li><a href="/contact">contact</a></li>
+            <li><a href="blog">blog</a></li>
         </ul>
     </nav>
   )
